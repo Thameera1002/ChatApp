856603603f576664a81273367461bd0aff0148d6
Thameera1002 <thameera1002@gmail.com>
2022-10-31 23:00:11 +0530

    LoginForm added

---
+++ src/com/tm/chat_app/controller/LoginFormController.java
@@ -0,0 +1,11 @@
+package com.tm.chat_app.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
+public class LoginFormController {
+    public JFXTextField tfUserName;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+}
