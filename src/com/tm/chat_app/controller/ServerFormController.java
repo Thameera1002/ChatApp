f2e5050123e523a327673d4884aef550126a3899
Thameera1002 <thameera1002@gmail.com>
2022-10-31 22:33:22 +0530

    Required libs added, serverform items init.

---
+++ src/com/tm/chat_app/controller/ServerFormController.java
@@ -0,0 +1,13 @@
+package com.tm.chat_app.controller;
+
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class ServerFormController {
+
+    public TextField txtMessageBox;
+
+    public void shutdownServerOnClick(MouseEvent mouseEvent) {
+    }
+}
+
