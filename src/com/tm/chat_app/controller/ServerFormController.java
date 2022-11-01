d2f0f2a71764e0848bca52d8582e8b5f22024bbc
Thameera1002 <thameera1002@gmail.com>
2022-11-01 22:26:07 +0530

    ServerFormController updated.

--- src/com/tm/chat_app/controller/ServerFormController.java
+++ src/com/tm/chat_app/controller/ServerFormController.java
@@ -30,6 +30,7 @@ public class ServerFormController {
                 ServerSocket serverSocket = new ServerSocket(5000);
 
                 System.out.println("Server Started...");
+                displayMessageOnRight("Server Started...");
                 server = new Server(serverSocket);
                 server.startServer(vbox_msgs);
             } catch (IOException e) {
@@ -41,7 +42,7 @@ public class ServerFormController {
         });
     }
 
-    public static void displayMessageOnRight(String messageToSend, VBox vBox){
+    public static void displayMessageOnRight(String messageToSend){
         if(!messageToSend.isEmpty()){
             HBox hBox = new HBox();
             hBox.setAlignment(Pos.CENTER_RIGHT);
@@ -52,9 +53,6 @@ public class ServerFormController {
             textFlow.setPadding(new Insets(5,5,5,10));
             msgText.setFill(Color.WHITE);
             hBox.getChildren().add(textFlow);
-            Platform.runLater(()->{
-                vBox.getChildren().add(hBox);
-            });
         }
     }
 
