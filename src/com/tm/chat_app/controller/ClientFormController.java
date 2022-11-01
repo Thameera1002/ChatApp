9fb06f8cad1426773fdbf7e3a60f2f90e25416f2
Thameera1002 <thameera1002@gmail.com>
2022-11-01 19:53:01 +0530

    Client updated, setClientName()

--- src/com/tm/chat_app/controller/ClientFormController.java
+++ src/com/tm/chat_app/controller/ClientFormController.java
@@ -1,15 +1,36 @@
 package com.tm.chat_app.controller;
 
+import com.tm.chat_app.utill.Client;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.Socket;
 
 public class ClientFormController {
 
+    public TextField txtMessageBox;
+    public VBox vbox_msg;
     String clientName;
+    public Client client;
+
+    public static VBox senderVBox;
 
     public void initialize(){
         System.out.println("Initialize");
     }
     public void setClientName(String name){
+        new Thread(()->{
+            //set client name
+            try {
+                senderVBox = vbox_msg;
+                client = new Client(new Socket("localhost",5000),name,vbox_msg);
+                System.out.println("Connected to the server...");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
         clientName=name;
         System.out.println(clientName);
     }
