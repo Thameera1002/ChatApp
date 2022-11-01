53047d4942bf5ff135cdbe85ba67faa4bef15134
Thameera1002 <thameera1002@gmail.com>
2022-11-01 20:35:51 +0530

    ClientFormController, displayMessageOnRight() updated.

--- src/com/tm/chat_app/controller/ClientFormController.java
+++ src/com/tm/chat_app/controller/ClientFormController.java
@@ -1,10 +1,17 @@
 package com.tm.chat_app.controller;
 
 import com.tm.chat_app.utill.Client;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 import java.io.IOException;
 import java.net.Socket;
@@ -37,5 +44,22 @@ public class ClientFormController {
         System.out.println(clientName);
     }
     public void exitClientOnClick(MouseEvent mouseEvent) {
+    }
+
+    public static void displayMessageOnRight(String messageToSend, VBox vBox){
+        if(!messageToSend.isEmpty()){
+            HBox hBox = new HBox();
+            hBox.setAlignment(Pos.CENTER_RIGHT);
+            hBox.setPadding(new Insets(5,5,5,10));
+            Text msgText = new Text(messageToSend);
+            TextFlow textFlow   = new TextFlow(msgText);
+            textFlow.setStyle("-fx-background-color: #16a085; -fx-background-radius: 10 10 0 10");
+            textFlow.setPadding(new Insets(5,5,5,10));
+            msgText.setFill(Color.WHITE);
+            hBox.getChildren().add(textFlow);
+            Platform.runLater(()->{
+                vBox.getChildren().add(hBox);
+            });
+        }
     }
 }
