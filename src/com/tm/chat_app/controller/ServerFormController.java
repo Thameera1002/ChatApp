7f1e4e918721e20f039eb87d66fdbc3ccdfd8d77
Thameera1002 <thameera1002@gmail.com>
2022-11-01 22:18:50 +0530

    ClientHandler & ServerFormController updated.

--- src/com/tm/chat_app/controller/ServerFormController.java
+++ src/com/tm/chat_app/controller/ServerFormController.java
@@ -1,19 +1,28 @@
 package com.tm.chat_app.controller;
 
+import com.tm.chat_app.utill.Server;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 
 public class ServerFormController {
 
+    public ScrollPane scrollPane;
+    public VBox vbox_msgs;
+
+    private Server server;
     public void initialize(){
         new Thread(()->{
             try {
                 ServerSocket serverSocket = new ServerSocket(5000);
+
                 System.out.println("Server Started...");
-                serverSocket.accept();
+                server = new Server(serverSocket);
+                server.startServer(vbox_msgs);
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- src/com/tm/chat_app/utill/ClientHandler.java
+++ src/com/tm/chat_app/utill/ClientHandler.java
@@ -2,9 +2,7 @@ package com.tm.chat_app.utill;
 
 import javafx.scene.layout.VBox;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 
@@ -18,6 +16,30 @@ public class ClientHandler implements Runnable{
 
     private VBox vBox;
     public String userName;
+
+    public ClientHandler(Socket socket,VBox vBox){
+        try {
+            this.socket = socket;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.printWriter = new PrintWriter(socket.getOutputStream(),true);
+            this.userName= bufferedReader.readLine();
+            this.vBox=vBox;
+            allClients.add(this);
+        }catch (IOException e){
+
+        }
+    }
+
+    public void closeAll(Socket socket ,BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+        try {
+            if(bufferedReader != null) bufferedReader.close();
+            if (bufferedWriter != null) bufferedWriter.close();
+            if(socket != null) socket.close();
+        }catch (Exception e){
+            closeAll(this.socket,this.bufferedReader,this.bufferedWriter);
+        }
+    }
 
     @Override
     public void run() {
