74db8733063bde30565625268f123a9a0f3ed328
Thameera1002 <thameera1002@gmail.com>
2022-11-01 21:47:36 +0530

    Server.java init

---
+++ src/com/tm/chat_app/utill/Server.java
@@ -0,0 +1,33 @@
+package com.tm.chat_app.utill;
+
+import javafx.scene.layout.VBox;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private final ServerSocket serverSocket;
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    private boolean ifShutdown;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+        this.ifShutdown=false;
+    }
+
+    public void startServer(VBox server_vBox){
+        try {
+            socket = serverSocket.accept();
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //=====> client handler ===> static method
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
